556f6591347cad159ae8885d6d0e22b3235dec69
lindsey <lindsey@makers.tech>
2019-06-07 14:18:38 +0100

    Add the call to twitter

---
+++ index.js
@@ -0,0 +1,5 @@
+const makersTwitter = require('./makers-twitter')
+
+console.log('hello')
+
+makersTwitter.callTwitter()
---
+++ makers-twitter.js
@@ -0,0 +1,28 @@
+const  Twitter = require('twitter');
+const config = require('./config.js');
+
+const T = new Twitter(config);
+const token = config.slack_api_key; 
+
+var params = {
+  q: '#makersacademy',
+  count: 10,
+  result_type: 'recent',
+  lang: 'en'
+}
+
+function handleResponseFromTwitter(err,data,response) {
+    if(!err){
+      console.log(data);
+  } else {
+    console.error(err);
+  }
+}
+
+function callTwitter() {
+  T.get('search/tweets', params, handleResponseFromTwitter
+)};
+
+module.exports = {
+  callTwitter: callTwitter
+}
